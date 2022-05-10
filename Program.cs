6fb155ac84e9f5df08b61e9a42067af16ffa7b92
SwivelToons <ttvbrybry@gmail.com>
2022-05-10 13:14:38 -0700

    Adding some files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fortnite
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
